0160d1d43127570a52b48b2622ee2641d791fbbf
aleksnick <aleksnick@list.ru>
2012-09-27 18:14:32 +0400

    add settings

--- plugins/pl_baseFdb.php
+++ plugins/pl_baseFdb.php
@@ -1,12 +1,14 @@
 <?php class pl_baseFdb {
+
 function engine(&$q) {
-	//if ($q->ajax!="TRUE") {
-		if ((isset($q->fdb_dbname)) && (isset($q->fdb_login)) && (isset($q->fdb_password))) {
+	if ((isset($q->fdb_dbname)) && (isset($q->fdb_login)) && (isset($q->fdb_password))) {
+		if (($q->fdb_dbname!="") && ($q->fdb_login!="") && ($q->fdb_password!="")) {
 			$q->fdb_db=@ibase_connect($q->fdb_dbname,$q->fdb_login,$q->fdb_password);
 			if (isset ($q->fdb_db)) {
 				$q->fdb_it=@ibase_trans(IBASE_WRITE+IBASE_COMMITTED+IBASE_REC_VERSION+IBASE_NOWAIT,$q->fdb_db);
 			}
 		}
-	//}
+	}
 }
+
 } ?>
